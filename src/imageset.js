95d295f32725629512cfa5d87c35c011b35eac86
Stoney-FD <johannesstein@freeze-dev.com>
2014-12-16 21:23:46 +0000

    Rough draft

---
+++ src/imageset.js
@@ -0,0 +1,29 @@
+(function(exports) {
+  
+  /**
+   * Converts a single element
+   */
+  var convert = function(element) {
+    // Get data-src attribute
+    var attribute = element.getAttribute('data-src');
+    
+    // Try to convert
+    try {
+      
+    } catch(e) {
+      
+    }
+  };
+  
+  /**
+   * 
+   * 
+   */
+  var imageset = function(nodeList) {
+    
+  };
+  
+  
+  exports.imageset = imageset;
+  
+})(window);
